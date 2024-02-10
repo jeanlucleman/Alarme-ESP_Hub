b91efc50e499ae6d9498d2b5026add9c4396a2b9
Jean-Luc <jean-luc.leman@laposte.net>
2024-02-10 08:20:03 +0100

    New project

---
+++ include/version.h
@@ -0,0 +1,10 @@
+
+#ifndef BUILD_NUMBER
+  #define BUILD_NUMBER "615"
+#endif
+#ifndef VERSION
+  #define VERSION "v1.1.615 - 2024-02-10 08:10:33.641360"
+#endif
+#ifndef VERSION_SHORT
+  #define VERSION_SHORT "v1.1.615"
+#endif
